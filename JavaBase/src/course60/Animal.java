f5b0ad854ffda4e43cd10d2373ae0e6f3263e859
S31 <S31@MSI>
2023-09-19 10:32:39 +0900

    ポリモーフィズムとオーバーライドのプログラム #2

---
+++ JavaBase/src/course60/Animal.java
@@ -0,0 +1,7 @@
+package course60;
+
+public class Animal {
+	public void nakigoe() {
+		System.out.println("鳴き声を出します。");
+	}
+}
---
+++ JavaBase/src/course60/UseClass.java
@@ -0,0 +1,12 @@
+package course60;
+
+public class UseClass {
+	public static void main(String[] args) {
+		Animal dog = new Dog();
+		Animal cat = new Cat();
+
+		//ぽりもーふぃずむ
+		dog.nakigoe();
+        cat.nakigoe();
+	}
+}
